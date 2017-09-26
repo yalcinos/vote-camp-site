f85d155834e769615392bf182439716418103769
yalcin tatar <yalcintatar41@gmail.com>
2017-09-26 19:12:58 +0000

    Added submit page template

--- app.js
+++ app.js
@@ -2,13 +2,7 @@ var express=require("express");
 var app=express();
 app.set("view engine","ejs");
 var bodyParser=require("body-parser");
-
-
 app.use(bodyParser.urlencoded({extended:true}));
-app.get("/",function(req,res){
-	res.render("mainpage");
-});
-app.get("/campgrounds",function(req,res){
 	var campgrounds = [
         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
@@ -21,12 +15,25 @@ app.get("/campgrounds",function(req,res){
         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
 ];
 
+
+app.get("/",function(req,res){
+	res.render("mainpage");
+});
+app.get("/campgrounds",function(req,res){
+
 		res.render("campgrounds",{campingsite:campgrounds});
+});
+app.get("/campgrounds/new",function(req, res) {
+    res.render("new.ejs");
 });
 app.post("/campgrounds",function(req,res){
 	//get data form and add array.
 	//redirect campground.
-	res.send("you hit post route");
+	var name=req.body.name;
+	var img=req.body.img;
+	var newCampObj={name:name,image:img};
+	campgrounds.push(newCampObj);
+	res.redirect("/campgrounds");
 });
 
 app.listen(process.env.PORT, process.env.IP,function(){
