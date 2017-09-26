d1d3ec7c8c4ec038d1c414907e78d33b717b9cbd
yalcin tatar <yalcintatar41@gmail.com>
2017-09-26 16:13:34 +0000

    added body-parser packeage

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express=require("express");
 var app=express();
 app.set("view engine","ejs");
+var bodyParser=require("body-parser");
 
 
-
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req,res){
 	res.render("mainpage");
 });
