a1e0a4e45289c9564f7e9d321748c8e307b02317
yalcin tatar <yalcintatar41@gmail.com>
2017-09-29 18:02:30 +0000

    MongoDB database added and insert function added in post route

--- app.js
+++ app.js
@@ -1,42 +1,64 @@
-var express=require("express");
-var app=express();
-app.set("view engine","ejs");
-var bodyParser=require("body-parser");
+var express=require("express"),
+    app=express(),
+    bodyParser=require("body-parser"),
+    mongoose=require("mongoose");
+
+mongoose.connect("mongodb://localhost/vote_places",{useMongoClient: true});    
 app.use(bodyParser.urlencoded({extended:true}));
-	var campgrounds = [
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},    
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"}
-];
+app.set("view engine","ejs");
 
+var placesSchema=new mongoose.Schema({
+   name:String,
+   img:String
+});
+var Places=mongoose.model("Places",placesSchema);
 
+// Places.create(
+//     {
+//         name: "Granite Hill",
+//         img: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"
+        
+//     },function(err,place){
+//     if(err){
+//         console.log("FAILED!");
+//     }else{
+//         console.log("Created Successful!");
+//         console.log(place);
+//     }
+// });
+	
 app.get("/",function(req,res){
 	res.render("mainpage");
 });
-app.get("/campgrounds",function(req,res){
 
-		res.render("campgrounds",{campingsite:campgrounds});
+app.get("/campgrounds",function(req,res){
+    Places.find({},function(err,place){
+        if(err){
+            console.log("Error is:"+err);
+        }else{
+            console.log("DATA FETCH SUCCESSFULLY!");
+           	res.render("campgrounds",{campingsite:place});
+        }
+    });
 });
+
 app.get("/campgrounds/new",function(req, res) {
     res.render("new.ejs");
 });
+
 app.post("/campgrounds",function(req,res){
 	//get data form and add array.
 	//redirect campground.
 	var name=req.body.name;
 	var img=req.body.img;
-	var newCampObj={name:name,image:img};
-	campgrounds.push(newCampObj);
-	res.redirect("/campgrounds");
+	var newPlaceObj={name:name,img:img};
+	Places.create(newPlaceObj,function(err,newlyCreatedPlaces){
+	    if(err){
+	        console.log(err);
+	    }else{
+	        res.redirect("/campgrounds");
+	    }
+	});
 });
 
 app.listen(process.env.PORT, process.env.IP,function(){
