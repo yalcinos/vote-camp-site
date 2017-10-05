bfd47c9d556f18ca4054a939db24de01fbb5e8db
yalcintatar <yalcintatar41@gmail.com>
2017-10-05 16:23:31 +0300

    nodes modified.

--- app.js
+++ app.js
@@ -75,6 +75,6 @@ app.get("/campgrounds/:id",function(req, res) {
     });
 });
 
-app.listen(process.env.PORT, process.env.IP,function(){
+app.listen(3001,function(){
 	console.log("Server has started.");
 });
