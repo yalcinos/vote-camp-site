6e0afe1e0c61e02e075f46b14c094d689f5d4759
yalcintatar <yalcintatar41@gmail.com>
2017-10-06 14:50:49 +0300

    Added comments array in Places model

--- models/places.js
+++ models/places.js
@@ -2,7 +2,13 @@ var mongoose=require("mongoose");
 var placesSchema=new mongoose.Schema({
    name:String,
    img:String,
-   description:String
+   description:String,
+   comments:[
+       {
+          type:mongoose.Schema.Types.ObjectId,
+          ref:"Comment"
+       }
+   ]
 });
 
 module.exports=mongoose.model("Places",placesSchema);
