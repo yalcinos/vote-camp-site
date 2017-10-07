8d1130ce2f65dd1dc918336a1b6d157716dbae11
yalcintatar <yalcintatar41@gmail.com>
2017-10-07 16:23:36 +0300

    UI improvement

--- seed.js
+++ seed.js
@@ -6,17 +6,17 @@ function seedDB() {
         {
             title:"Historical Place 1",
             img:"https://farm2.staticflickr.com/1616/25936335344_5ba498ba15.jpg",
-            description:"blah blah blah"
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed felis vitae turpis consequat consequat. Quisque in condimentum nulla. Quisque ut hendrerit elit. Vestibulum vulputate hendrerit enim eu sagittis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum nibh tortor, tincidunt at libero id, maximus porta neque. Integer eu dictum velit, sollicitudin ultrices tortor. Praesent at hendrerit lectus. Praesent diam turpis, sagittis sed risus ut, imperdiet porttitor neque. Aenean vel sodales justo. Donec congue metus eu velit feugiat varius. Fusce placerat felis et lorem placerat interdum. Nunc posuere sapien scelerisque aliquam pharetra. Curabitur risus velit, pulvinar sed feugiat eget, placerat sit amet tellus. Mauris volutpat condimentum elit, at dapibus ipsum."
         },
         {
             title:"Historical Place 2",
             img:"https://farm4.staticflickr.com/3172/2355138562_faaf7222dc.jpg",
-            description:"blah blah blah"
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed felis vitae turpis consequat consequat. Quisque in condimentum nulla. Quisque ut hendrerit elit. Vestibulum vulputate hendrerit enim eu sagittis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum nibh tortor, tincidunt at libero id, maximus porta neque. Integer eu dictum velit, sollicitudin ultrices tortor. Praesent at hendrerit lectus. Praesent diam turpis, sagittis sed risus ut, imperdiet porttitor neque. Aenean vel sodales justo. Donec congue metus eu velit feugiat varius. Fusce placerat felis et lorem placerat interdum. Nunc posuere sapien scelerisque aliquam pharetra. Curabitur risus velit, pulvinar sed feugiat eget, placerat sit amet tellus. Mauris volutpat condimentum elit, at dapibus ipsum."
         },
         {
             title:"Historical Place 3",
             img:"https://farm3.staticflickr.com/2041/32459432790_03606aa61a.jpg",
-            description:"blah blah blah"
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed felis vitae turpis consequat consequat. Quisque in condimentum nulla. Quisque ut hendrerit elit. Vestibulum vulputate hendrerit enim eu sagittis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum nibh tortor, tincidunt at libero id, maximus porta neque. Integer eu dictum velit, sollicitudin ultrices tortor. Praesent at hendrerit lectus. Praesent diam turpis, sagittis sed risus ut, imperdiet porttitor neque. Aenean vel sodales justo. Donec congue metus eu velit feugiat varius. Fusce placerat felis et lorem placerat interdum. Nunc posuere sapien scelerisque aliquam pharetra. Curabitur risus velit, pulvinar sed feugiat eget, placerat sit amet tellus. Mauris volutpat condimentum elit, at dapibus ipsum."
         }
     ];
     Places.remove({},function (err) {
